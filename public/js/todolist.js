d278cfedf9e1e74ec0c489da1e14da3b0e1e83f0
jakubowska09 <mellepatrycia@gmail.com>
2018-11-21 11:36:54 +0100

    exo7 button delete

--- public/js/todolist.js
+++ public/js/todolist.js
@@ -4,8 +4,9 @@ let faire = document.getElementById("all");
 let aujoudhui = document.getElementById("todo");
 let donetermine = document.getElementById("done");
 let cot = document.querySelector(".contour")
-let edit = document.querySelector(".edit")
 let terminer = document.querySelector(".termine")
+let input = document.querySelector(".text-center");
+
 
 
 
@@ -19,20 +20,23 @@ function publier() {
 
 
     var nom = document.getElementById("list").value
-    taches.innerHTML = '<div>' + nom + '</div>' + '<div><i class="fas fa-check check"></i><i class="fas fa-pencil-alt edit"></i><i class="fas fa-trash-alt termine"></i></div> ';
+    taches.innerHTML = '<div>' + nom + '</div>' + '<div><i class="fas fa-check check"></i><i class="fas fa-pencil-alt edit"></i><i class="fas fa-trash-alt termine"></i> <i class="fas fa-lock lock"></i><i class="fal fa-eraser gommme"></i></div> ';
 
 
     if (nom != "") {
         console.log("ok")
         divTach.appendChild(taches).classList.add("tacheee", "contour");
+
     }
     else {
         console.log("le champs est obligatoire");
         document.querySelector("#list").placeholder = "Le champs est obligatoire";
-        placeholder.style.color = "red";
+        input.classList.add("error");
+
+        console.log(input);
 
     }
-
+    //  button valide
     let valide = document.querySelector(".check");
 
     function confirmer() {
@@ -50,6 +54,24 @@ function publier() {
     }
     console.log(valide);
     valide.addEventListener("click", confirmer);
+    // buttom delete
+    let q = document.querySelector(".termine")
+    function enlever() {
+        q.addEventListener("click", () => {
+            divTach.removeChild(taches);
+
+        })
+    }
+    q.addEventListener("click", enlever);
+    // button edit
+    l
+    let edit = document.querySelector(".edit");
+    let kaka = document.querySelector(".lock");
+    let gom = document.querySelector(".gommme");
+    function modifier() {
+
+    }
+
 
 }
 cadre.addEventListener("click", publier);
